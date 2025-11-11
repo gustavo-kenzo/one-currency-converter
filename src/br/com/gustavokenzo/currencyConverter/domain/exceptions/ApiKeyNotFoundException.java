c035a99734be2354f4fffa56e2ae47081e83f671
Gustavo <sgkenzo212007@gmail.com>
2025-11-11 16:40:26 -0300

    📝 docs: Update message to display README https://github.com/gustavo-kenzo/one-currency-converter
    ApiKeyNotFoundException message now directs the user to the README (which guides the user through setting environment variables)

--- src/br/com/gustavokenzo/currencyConverter/domain/exceptions/ApiKeyNotFoundException.java
+++ src/br/com/gustavokenzo/currencyConverter/domain/exceptions/ApiKeyNotFoundException.java
@@ -3,6 +3,6 @@ package br.com.gustavokenzo.currencyConverter.domain.exceptions;
 public class ApiKeyNotFoundException extends CurrencyConverterException {
 
     public ApiKeyNotFoundException() {
-        super("API key not found in environment variables", "Configure the API key in your operating system's environment variables. README: xxxxxx");
+        super("API key not found in environment variables", "Configure the API key in your operating system's environment variables. README: https://github.com/gustavo-kenzo/one-currency-converter");
     }
 }
